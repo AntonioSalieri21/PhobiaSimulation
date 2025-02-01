4bf5d6cd243d817f536b6021b7adeb5c677379ea
AntonioSalieri21 <62499991+AntonioSalieri21@users.noreply.github.com>
2025-02-01 20:38:36 +0100

    Transfered project to C++

---
+++ Source/VRProject/TestCPP.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestCPP.h"
+
+// Sets default values
+ATestCPP::ATestCPP()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestCPP::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestCPP::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 20.f, FColor::Red, FString::Printf(TEXT("TEST")));
+	}
+
+}
+
+void ATestCPP::SpawnActor()
+{
+
+
+
+}
+
